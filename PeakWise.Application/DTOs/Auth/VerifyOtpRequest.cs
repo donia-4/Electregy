6258ef9f219aff9feb78d5a96c46f086b4bc7587
Donia Shaban <doniashaban723@gmail.com>
2026-04-18 02:54:04 +0200

    Authintications and tokens DTOs

---
+++ PeakWise.Application/DTOs/Auth/VerifyOtpRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakWise.Application.DTOs.Auth
+{
+    public class VerifyOtpRequest
+    {
+        public string UserId { get; set; }
+        public string Otp { get; set; }
+    }
+}
---
+++ PeakWise.Application/DTOs/Tokens/TokenResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakWise.Application.DTOs.Tokens
+{
+    public class TokenResponse
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
