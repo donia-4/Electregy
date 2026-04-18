e144b883ff6b6c4dcc1a7ddd1f88e19c41cc72b1
Donia Shaban <doniashaban723@gmail.com>
2026-04-18 02:55:09 +0200

    Email, OTP and token services and interfaces.

---
+++ PeakWise.Application/Interfaces/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace PeakWise.Application.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendOtpEmailAsync(AppUser applicationUser, string otp);
+    }
+}
