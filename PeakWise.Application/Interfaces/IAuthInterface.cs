7ab7b7c43c25fc6cb547702aca96d09ab329df17
Donia Shaban <doniashaban723@gmail.com>
2026-04-18 02:54:34 +0200

    Authintication Service and interface

---
+++ PeakWise.Application/Interfaces/IAuthInterface.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+using PeakWise.Application.DTOs.Auth;
+using PeakWise.Shared.Responses;
+
+namespace PeakWise.Application.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<Response<RegisterResponse>> RegisterAsync(RegisterRequest request);
+        Task<Response<LoginResponse>> LoginAsync(LoginRequest loginRequest);
+        Task<Response<string>> LogoutAsync(ClaimsPrincipal userClaims);
+    }
+}
