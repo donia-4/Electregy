a551bc6fb0e510654a2496973e82644ba0c884d2
Donia Shaban <doniashaban723@gmail.com>
2026-04-18 02:57:43 +0200

    Added RefreshTokens Domain Model and its migration and added it in the AppDbContext

---
+++ PeakWise.Domain/Entities/Tokens/UserRefreshTokens.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakWise.Domain.Entities.Tokens
+{
+    public class UserRefreshToken
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public string? Token { get; set; }
+        public bool IsUsed { get; set; }
+        public DateTime ExpiryDateUtc { get; set; }
+        public virtual AppUser? User { get; set; }
+    }
+}
