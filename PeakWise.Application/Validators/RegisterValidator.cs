b8d8fc41a88ec4d15a2a6838cdb0979c194578fd
Donia Shaban <doniashaban723@gmail.com>
2026-04-18 02:59:07 +0200

    added authentication validators

---
+++ PeakWise.Application/Validators/RegisterValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Identity.Data;
+
+public class RegisterValidator : AbstractValidator<PeakWise.Application.DTOs.Auth.RegisterRequest>
+{
+    public RegisterValidator()
+    {
+        RuleFor(x => x.FullName)
+            .NotEmpty().WithMessage("User Name is required.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email format.");   
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
+    }
+}
---
+++ PeakWise.Infrastructure/Migrations/20260417221534_added_UserRefreshTokens_table.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PeakWise.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_UserRefreshTokens_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserRefreshTokens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsUsed = table.Column<bool>(type: "bit", nullable: false),
+                    ExpiryDateUtc = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRefreshTokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserRefreshTokens_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRefreshTokens_UserId",
+                table: "UserRefreshTokens",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserRefreshTokens");
+        }
+    }
+}
