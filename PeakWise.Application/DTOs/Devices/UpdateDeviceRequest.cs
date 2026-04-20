f091f79ffa0495ad8fa89c20a989ca564ea201bf
Donia Shaban <doniashaban723@gmail.com>
2026-04-20 07:15:18 +0200

    Dtos considering the upadte and Device Response

---
+++ PeakWise.Application/DTOs/Devices/UpdateDeviceRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+using PeakWise.Domain.Enums;
+
+namespace PeakWise.Application.DTOs.Devices
+{
+    public class UpdateDeviceRequest
+    {
+        public int Id { get; set; } // Id is required to find the device
+
+        public string? Name { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public DeviceType? Type { get; set; }
+        public double? Watts { get; set; }
+        public double? HoursPerDay { get; set; }
+    }
+}
