d55b89dfc604131d05b75434f0194cc9de131c3f
Donia Shaban <doniashaban723@gmail.com>
2026-04-25 19:11:09 +0300

    consumtion module

---
+++ PeakWise.Application/DTOs/Consumption/ReadingResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeakWise.Application.DTOs.Consumption
+{
+    public class ReadingResponse
+    {
+        public int Id { get; set; }
+        public double Watts { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
